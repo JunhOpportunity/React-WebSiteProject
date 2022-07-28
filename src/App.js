b80a1a40b041f1ac7ab3366f9cff1c99491a2f70
JunHo Kim <89464762+JunhOpportunity@users.noreply.github.com>
2022-07-28 15:55:33 +0900

    React Master Basic

--- src/App.js
+++ src/App.js
@@ -1,10 +1,43 @@
 import "./styles.css";
+import styled, { keyframes } from "styled-components";
+
+const Father = styled.div`
+  display: flex;
+`;
+
+const Box = styled.div`
+  width: 200px;
+  height: 200px;
+  background-color: tomato;
+`;
+
+const Text = styled.span`
+  color: white;
+`;
+
+const Circle = styled(Box)`
+  border-radius: 50px;
+`;
+
+const Btn = styled.button`
+  color: white;
+  background-color: tomato;
+  border: 0;
+  border-radius: 15px;
+`;
+
+const Input = styled.input.attrs({ required: true })`
+  background-color: tomato;
+`;
 
 export default function App() {
   return (
-    <div className="App">
-      <h1>Hello CodeSandbox</h1>
-      <h2>Start editing to see some magic happen!</h2>
-    </div>
+    <Father>
+      <Box bgColor="teal">
+        <Text>Hello</Text>
+      </Box>
+      <Circle bgColor="tomato" />
+      <Btn>Log in</Btn>
+    </Father>
   );
 }
