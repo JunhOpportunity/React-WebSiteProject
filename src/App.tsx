8cd72026dc53125463762376545c167a22a1cdab
JunHo Kim <89464762+JunhOpportunity@users.noreply.github.com>
2022-07-31 11:55:03 +0900

    Ts React Using 1

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import "./styles.css";
+import Circle from "./Circle";
+
+export default function App() {
+  return (
+    <div>
+      <Circle bgColor="teal" borderColor="white" />
+      <Circle text="ok" bgColor="tomato" />
+    </div>
+  );
+}
