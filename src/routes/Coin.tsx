92e29ce5949141a248d1c7047bba40e205903586
JunHo Kim <89464762+JunhOpportunity@users.noreply.github.com>
2022-08-02 20:38:01 +0900

    React Coin Project 2

--- src/routes/Coin.tsx
+++ src/routes/Coin.tsx
@@ -33,16 +33,39 @@ interface RouteState {
   name: string;
 }
 
+interface LocationState {
+  state: {
+    name: string;
+    rank: number;
+  };
+}
+
 function Coin() {
   const [loading, setLoading] = useState(true);
-  const { coinId } = useParams<RouteParams>(); // useParams 쓰는 순간 type이 string or undefined
-  const { state } = useLocation<RouteState>();
+  const { coinId } = useParams(); // useParams 쓰는 순간 type이 string or undefined
+  const { state } = useLocation() as LocationState;
+  const [info, setInfo] = useState({});
+  const [priceInfo, setPriceInfo] = useState({});
+  interface InfoData {}
+  interface PriceData {}
+  useEffect(() => {
+    (async () => {
+      const infoData = await (
+        await fetch(`https://api.coinpaprika.com/v1/coins/${coinId}`)
+      ).json();
+      const priceData = await (
+        await fetch(`https://api.coinpaprika.com/v1/tickers/${coinId}`)
+      ).json();
+      setInfo(infoData);
+      setPriceInfo(priceData);
+    })();
+  });
   return (
     <Container>
       <Header>
-        <Title>{state.name}</Title>
+        <Title>{state?.name || "Loading..."}</Title>
       </Header>
-      {loading ? <Loader>"Loading...."</Loader> : null}
+      {loading ? <Loader>"Loading...."</Loader> : <span>{info.hello}</span>}
     </Container>
   );
 }
